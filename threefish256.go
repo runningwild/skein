59d649c61be77bbfff55ebe42a0e409857f72797
Jonathan Wills <runningwild@gmail.com>
2017-01-21 17:31:30 -0500

    optimized decryption

--- threefish256.go
+++ threefish256.go
@@ -11,22 +11,94 @@ type block256 struct {
 func (b *block256) encrypt() {
 	encryptBlock256(&b.state, &b.key, &b.tweak)
 }
+
+func (b *block256) decrypt() {
+	decryptBlock256(&b.state, &b.key, &b.tweak)
+}
+
 func encryptBlock256(state *[4]uint64, key *[5]uint64, tweak *[3]uint64) {
 	key[4] = c240 ^ key[0] ^ key[1] ^ key[2] ^ key[3]
 	tweak[2] = tweak[0] ^ tweak[1]
 
-	var d int
-	for d = 0; d < 18; d++ {
-		keySched(d, state, key, tweak, &keyShifts, &tweakShifts)
-		fourRoundsA(state)
-		d++
-		keySched(d, state, key, tweak, &keyShifts, &tweakShifts)
-		fourRoundsB(state)
-	}
-	state[0] += key[d%5]
-	state[1] += key[(d+1)%5] + tweak[d%3]
-	state[2] += key[(d+2)%5] + tweak[(d+1)%3]
-	state[3] += key[(d+3)%5] + uint64(d)
+	keySched(0, state, key, tweak, &keyShifts, &tweakShifts)
+	fourRoundsA(state)
+	keySched(1, state, key, tweak, &keyShifts, &tweakShifts)
+	fourRoundsB(state)
+	keySched(2, state, key, tweak, &keyShifts, &tweakShifts)
+	fourRoundsA(state)
+	keySched(3, state, key, tweak, &keyShifts, &tweakShifts)
+	fourRoundsB(state)
+	keySched(4, state, key, tweak, &keyShifts, &tweakShifts)
+	fourRoundsA(state)
+	keySched(5, state, key, tweak, &keyShifts, &tweakShifts)
+	fourRoundsB(state)
+	keySched(6, state, key, tweak, &keyShifts, &tweakShifts)
+	fourRoundsA(state)
+	keySched(7, state, key, tweak, &keyShifts, &tweakShifts)
+	fourRoundsB(state)
+	keySched(8, state, key, tweak, &keyShifts, &tweakShifts)
+	fourRoundsA(state)
+	keySched(9, state, key, tweak, &keyShifts, &tweakShifts)
+	fourRoundsB(state)
+	keySched(10, state, key, tweak, &keyShifts, &tweakShifts)
+	fourRoundsA(state)
+	keySched(11, state, key, tweak, &keyShifts, &tweakShifts)
+	fourRoundsB(state)
+	keySched(12, state, key, tweak, &keyShifts, &tweakShifts)
+	fourRoundsA(state)
+	keySched(13, state, key, tweak, &keyShifts, &tweakShifts)
+	fourRoundsB(state)
+	keySched(14, state, key, tweak, &keyShifts, &tweakShifts)
+	fourRoundsA(state)
+	keySched(15, state, key, tweak, &keyShifts, &tweakShifts)
+	fourRoundsB(state)
+	keySched(16, state, key, tweak, &keyShifts, &tweakShifts)
+	fourRoundsA(state)
+	keySched(17, state, key, tweak, &keyShifts, &tweakShifts)
+	fourRoundsB(state)
+	keySched(18, state, key, tweak, &keyShifts, &tweakShifts)
+}
+func decryptBlock256(state *[4]uint64, key *[5]uint64, tweak *[3]uint64) {
+	key[4] = c240 ^ key[0] ^ key[1] ^ key[2] ^ key[3]
+	tweak[2] = tweak[0] ^ tweak[1]
+
+	reverseKeySched(18, state, key, tweak, &keyShifts, &tweakShifts)
+	reverseFourRoundsB(state)
+	reverseKeySched(17, state, key, tweak, &keyShifts, &tweakShifts)
+	reverseFourRoundsA(state)
+	reverseKeySched(16, state, key, tweak, &keyShifts, &tweakShifts)
+	reverseFourRoundsB(state)
+	reverseKeySched(15, state, key, tweak, &keyShifts, &tweakShifts)
+	reverseFourRoundsA(state)
+	reverseKeySched(14, state, key, tweak, &keyShifts, &tweakShifts)
+	reverseFourRoundsB(state)
+	reverseKeySched(13, state, key, tweak, &keyShifts, &tweakShifts)
+	reverseFourRoundsA(state)
+	reverseKeySched(12, state, key, tweak, &keyShifts, &tweakShifts)
+	reverseFourRoundsB(state)
+	reverseKeySched(11, state, key, tweak, &keyShifts, &tweakShifts)
+	reverseFourRoundsA(state)
+	reverseKeySched(10, state, key, tweak, &keyShifts, &tweakShifts)
+	reverseFourRoundsB(state)
+	reverseKeySched(9, state, key, tweak, &keyShifts, &tweakShifts)
+	reverseFourRoundsA(state)
+	reverseKeySched(8, state, key, tweak, &keyShifts, &tweakShifts)
+	reverseFourRoundsB(state)
+	reverseKeySched(7, state, key, tweak, &keyShifts, &tweakShifts)
+	reverseFourRoundsA(state)
+	reverseKeySched(6, state, key, tweak, &keyShifts, &tweakShifts)
+	reverseFourRoundsB(state)
+	reverseKeySched(5, state, key, tweak, &keyShifts, &tweakShifts)
+	reverseFourRoundsA(state)
+	reverseKeySched(4, state, key, tweak, &keyShifts, &tweakShifts)
+	reverseFourRoundsB(state)
+	reverseKeySched(3, state, key, tweak, &keyShifts, &tweakShifts)
+	reverseFourRoundsA(state)
+	reverseKeySched(2, state, key, tweak, &keyShifts, &tweakShifts)
+	reverseFourRoundsB(state)
+	reverseKeySched(1, state, key, tweak, &keyShifts, &tweakShifts)
+	reverseFourRoundsA(state)
+	reverseKeySched(0, state, key, tweak, &keyShifts, &tweakShifts)
 }
 
 var keyShifts = [85]int{
@@ -68,9 +140,17 @@ func keySched(d int, state *[4]uint64, key *[5]uint64, tweak *[3]uint64, keyShif
 	state[2] += key[keyShifts[d+2]] + tweak[tweakShifts[d+1]]
 	state[3] += key[keyShifts[d+3]] + uint64(d)
 }
+func reverseKeySched(d int, state *[4]uint64, key *[5]uint64, tweak *[3]uint64, keyShifts, tweakShifts *[85]int) {
+	state[0] -= key[keyShifts[d]]
+	state[1] -= key[keyShifts[d+1]] + tweak[tweakShifts[d]]
+	state[2] -= key[keyShifts[d+2]] + tweak[tweakShifts[d+1]]
+	state[3] -= key[keyShifts[d+3]] + uint64(d)
+}
 func eightRounds(d int, state *[4]uint64, key *[5]uint64, tweak *[3]uint64, keyShifts, tweakShifts *[85]int) //{}
 func fourRoundsA(state *[4]uint64)
 func fourRoundsB(state *[4]uint64)
+func reverseFourRoundsA(state *[4]uint64)
+func reverseFourRoundsB(state *[4]uint64)
 func fourRoundsA_slow(state *[4]uint64) {
 	state[0] += state[1]
 	state[1] = ((state[1] << 14) | (state[1] >> (64 - 14))) ^ state[0]
@@ -114,38 +194,65 @@ func fourRoundsB_slow(state *[4]uint64) {
 	state[1] = ((state[1] << 32) | (state[1] >> (64 - 32))) ^ state[2]
 }
 
-func (b *block256) decrypt() {
-	b.key[4] = c240 ^ b.key[0] ^ b.key[1] ^ b.key[2] ^ b.key[3]
-	b.tweak[2] = b.tweak[0] ^ b.tweak[1]
+func reverseFourRoundsA_slow(state *[4]uint64) {
+	state[1] ^= state[2]
+	state[1] = (state[1] >> 37) | (state[1] << (64 - 37))
+	state[2] -= state[1]
+	state[3] ^= state[0]
+	state[3] = (state[3] >> 5) | (state[3] << (64 - 5))
+	state[0] -= state[3]
 
-	r := 18
-	b.state[0] -= b.key[r%5]
-	b.state[1] -= b.key[(r+1)%5] + b.tweak[r%3]
-	b.state[2] -= b.key[(r+2)%5] + b.tweak[(r+1)%3]
-	b.state[3] -= b.key[(r+3)%5] + uint64(r)
-	// Our loop will run four rounds at a time, since the key-schedule only comes into play on every
-	// fourth round.
-	for r := 17; r >= 0; r-- {
-		b.unpermute()
-		b.unmix(r*4 + 3)
-		b.unpermute()
-		b.unmix(r*4 + 2)
-		b.unpermute()
-		b.unmix(r*4 + 1)
-		b.unpermute()
-		b.unmix(r * 4)
+	state[3] ^= state[2]
+	state[3] = (state[3] >> 40) | (state[3] << (64 - 40))
+	state[2] -= state[3]
+	state[1] ^= state[0]
+	state[1] = (state[1] >> 23) | (state[1] << (64 - 23))
+	state[0] -= state[1]
 
-		b.state[0] -= b.key[r%5]
-		b.state[1] -= b.key[(r+1)%5] + b.tweak[r%3]
-		b.state[2] -= b.key[(r+2)%5] + b.tweak[(r+1)%3]
-		b.state[3] -= b.key[(r+3)%5] + uint64(r)
-	}
+	state[1] ^= state[2]
+	state[1] = (state[1] >> 57) | (state[1] << (64 - 57))
+	state[2] -= state[1]
+	state[3] ^= state[0]
+	state[3] = (state[3] >> 52) | (state[3] << (64 - 52))
+	state[0] -= state[3]
+
+	state[3] ^= state[2]
+	state[3] = (state[3] >> 16) | (state[3] << (64 - 16))
+	state[2] -= state[3]
+	state[1] ^= state[0]
+	state[1] = (state[1] >> 14) | (state[1] << (64 - 14))
+	state[0] -= state[1]
 }
 
-// func (b *block256) mix(d int) {
-// 	b.state[0], b.state[1] = mix256(d, 0, b.state[0], b.state[1])
-// 	b.state[2], b.state[3] = mix256(d, 1, b.state[2], b.state[3])
-// }
+func reverseFourRoundsB_slow(state *[4]uint64) {
+	state[1] ^= state[2]
+	state[1] = (state[1] >> 32) | (state[1] << (64 - 32))
+	state[2] -= state[1]
+	state[3] ^= state[0]
+	state[3] = (state[3] >> 32) | (state[3] << (64 - 32))
+	state[0] -= state[3]
+
+	state[3] ^= state[2]
+	state[3] = (state[3] >> 22) | (state[3] << (64 - 22))
+	state[2] -= state[3]
+	state[1] ^= state[0]
+	state[1] = (state[1] >> 58) | (state[1] << (64 - 58))
+	state[0] -= state[1]
+
+	state[1] ^= state[2]
+	state[1] = (state[1] >> 12) | (state[1] << (64 - 12))
+	state[2] -= state[1]
+	state[3] ^= state[0]
+	state[3] = (state[3] >> 46) | (state[3] << (64 - 46))
+	state[0] -= state[3]
+
+	state[3] ^= state[2]
+	state[3] = (state[3] >> 33) | (state[3] << (64 - 33))
+	state[2] -= state[3]
+	state[1] ^= state[0]
+	state[1] = (state[1] >> 25) | (state[1] << (64 - 25))
+	state[0] -= state[1]
+}
 
 func (b *block256) unmix(d int) {
 	b.state[0], b.state[1] = unmix256(d, 0, b.state[0], b.state[1])
