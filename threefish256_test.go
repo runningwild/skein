2fb77a88729950f8aa7c266bf8c3d849240d4c78
Jonathan Wills <runningwild@gmail.com>
2017-01-22 13:48:41 +0000

    fix tests to use ShouldResemble rather than ShouldEqual

--- threefish256_test.go
+++ threefish256_test.go
@@ -12,13 +12,13 @@ func TestEncryptAndDecrypt(t *testing.T) {
 	Convey("get the right answer for known inputs", t, func() {
 		var b block256
 		b.encrypt()
-		So(b.state, ShouldEqual, [4]uint64{0x94eeea8b1f2ada84, 0xadf103313eae6670, 0x952419a1f4b16d53, 0xd83f13e63c9f6b11})
+		So(b.state, ShouldResemble, [4]uint64{0x94eeea8b1f2ada84, 0xadf103313eae6670, 0x952419a1f4b16d53, 0xd83f13e63c9f6b11})
 		b.encrypt()
-		So(b.state, ShouldEqual, [4]uint64{0x35b93afdf2dc5f43, 0x3b2032fb6b123f71, 0x4631261fd3f22b56, 0x2097633f6034a5af})
+		So(b.state, ShouldResemble, [4]uint64{0x35b93afdf2dc5f43, 0x3b2032fb6b123f71, 0x4631261fd3f22b56, 0x2097633f6034a5af})
 		b.decrypt()
-		So(b.state, ShouldEqual, [4]uint64{0x94eeea8b1f2ada84, 0xadf103313eae6670, 0x952419a1f4b16d53, 0xd83f13e63c9f6b11})
+		So(b.state, ShouldResemble, [4]uint64{0x94eeea8b1f2ada84, 0xadf103313eae6670, 0x952419a1f4b16d53, 0xd83f13e63c9f6b11})
 		b.decrypt()
-		So(b.state, ShouldEqual, [4]uint64{0, 0, 0, 0})
+		So(b.state, ShouldResemble, [4]uint64{0, 0, 0, 0})
 	})
 }
 
